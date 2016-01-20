7138e04046cfacea5f23462030b500884e576063
Benson Huynh <quoc@shaw.ca>
2016-01-20 15:17:28 -0800

    final version

--- Weather/LHWAppDelegate.m
+++ Weather/LHWAppDelegate.m
@@ -23,24 +23,16 @@
     City *calgaryAb = [[City alloc] initWithName:@"Calgary" weatherConditionOfCity:@"snowing" temperatureOfCity:0 windSpeed:55];
     City *newYorkNy = [[City alloc] initWithName:@"New York" weatherConditionOfCity:@"foggy" temperatureOfCity:9 windSpeed:25];
     
-    CityViewController *vancouverViewController = [[CityViewController alloc] initWithCity:vancouverBc];
-    CityViewController *sanFranciscoViewController = [[CityViewController alloc] initWithCity:sanFranciscoCa];
-    CityViewController *torontoViewController = [[CityViewController alloc] initWithCity:torontoOn];
-    CityViewController *calgaryViewController = [[CityViewController alloc] initWithCity:calgaryAb];
-    CityViewController *newYorkViewController = [[CityViewController alloc] initWithCity:newYorkNy];
-    
     // Don't forget to assign the window a rootViewController
-    UINavigationController *vancouverNavCon = [[UINavigationController alloc] initWithRootViewController:vancouverViewController];
-    UINavigationController *sanFranciscoNavCon = [[UINavigationController alloc] initWithRootViewController:sanFranciscoViewController];
-    UINavigationController *torontoNavCon = [[UINavigationController alloc] initWithRootViewController:torontoViewController];
-    UINavigationController *calgaryNavCon = [[UINavigationController alloc] initWithRootViewController:calgaryViewController];
-    UINavigationController *newYorNavCon = [[UINavigationController alloc] initWithRootViewController:newYorkViewController];
+    UINavigationController *vancouverNavCon = [self navigationControllerForCity:vancouverBc];
+    UINavigationController *sanFranciscoNavCon = [self navigationControllerForCity:sanFranciscoCa];
+    UINavigationController *torontoNavCon = [self navigationControllerForCity:torontoOn];
+    UINavigationController *calgaryNavCon = [self navigationControllerForCity:calgaryAb];
+    UINavigationController *newYorkNavCon = [self navigationControllerForCity:newYorkNy];
+    NSArray *collectionOfCityNavCon = @[vancouverNavCon, torontoNavCon, calgaryNavCon, sanFranciscoNavCon, newYorkNavCon];
     
     UITabBarController *selectCityBar = [[UITabBarController alloc] init];
-    NSArray *collectionOfCityNavCon = @[vancouverNavCon, torontoNavCon, calgaryNavCon, sanFranciscoNavCon, newYorNavCon];
     selectCityBar.viewControllers = collectionOfCityNavCon;
-    
-    
     selectCityBar.tabBar.barTintColor = [UIColor blueColor];
     [[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont fontWithName:@"HelveticaNeue-Bold" size:12.0f], NSForegroundColorAttributeName : [UIColor whiteColor]}
                                              forState:UIControlStateNormal];
@@ -48,6 +40,12 @@
     [self.window makeKeyAndVisible];
     [self.window setRootViewController:selectCityBar];
     return YES;
+}
+
+-(UINavigationController *) navigationControllerForCity:(City *) city {
+    CityViewController *cityViewController = [[CityViewController alloc] initWithCity:city];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:cityViewController];
+    return navController;
 }
 
 @end
